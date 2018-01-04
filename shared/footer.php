e6a4aa30cc7ae9bee88bcbbfaeeea1fd3e1387f6
ming <minglun77127@gmail.com>
2018-01-04 16:42:10 -0500

    dev: web page segmentation

---
+++ shared/footer.php
@@ -0,0 +1,27 @@
+<div class="fbg">
+    <div class="fbg_resize">
+        <div class="col c1">
+            <h2><span>Other </span> houses</h2>
+            <a href="#"><img src="images/gal1.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal2.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal3.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal6.jpg" width="75" height="75" alt="" class="gal" /></a> </div>
+        <div class="col c3">
+            <h2><span>Contact</span> Us</h2>
+            <p>联系方式</p>
+            <p class="contact_info"> <span>Address:</span> 1458 TemplateAccess, USA<br />
+                <span>Telephone:</span> +123-1234-5678<br />
+                <span>FAX:</span> +458-4578<br />
+                <span>微信:</span> +301 - 0125 - 01258<br />
+                <span>E-mail:</span> <a href="#">mail@yoursitename.com</a> </p>
+        </div>
+        <div class="clr"></div>
+    </div>
+</div>
+<div class="footer">
+    <div class="footer_resize">
+        <p class="lf">Copyright &copy; <a href="#">Domain Name</a>. All Rights Reserved</p>
+        <p class="rf">Design by <a target="_blank" href="http://www.dreamtemplate.com/">DreamTemplate</a></p>
+        <div style="clear:both;"></div>
+    </div>
+</div>
+</div>
+</body>
+</html>
