a16b4d17d4884e6d8e6327a8d7c01fdc81575d09
Jinyue Song <jsong32@dons.usfca.edu>
2018-01-13 15:46:35 -0800

    initial website structure
    initialize the webpages for each sub website content branch

--- shared/header.php
+++ shared/header.php
@@ -30,14 +30,14 @@
             <div class="menu_nav">
                 <ul>
                     <li><a href="index.php"><span>主页</span></a></li>
-                    <li><a href="support.php"><span>优质楼盘</span></a></li>
-                    <li><a><span>公寓</span></a></li>
-                    <li><a><span>独栋联排</span></a></li>
-                    <li><a><span>豪宅</span></a></li>
-                    <li class="active"><a href="about.php"><span>已售楼盘</span></a></li>
+                    <li><a href="qualifiedProperty.php"><span>优质楼盘</span></a></li>
+                    <li><a href="condo.php"><span>公寓</span></a></li>
+                    <li><a href="townHouse.php"><span>独栋联排</span></a></li>
+                    <li><a href="luxury.php"><span>豪宅</span></a></li>
+                    <li><a href="sold.php"><span>已售楼盘</span></a></li>
                     <li><a href="blog.php"><span>客户反馈</span></a></li>
-                    <li><a href=""><span>市场走向</span></a></li>
-                    <li><a><span>买卖小助手</span></a></li>
+                    <li><a href="market.php"><span>市场走向</span></a></li>
+                    <li><a href="tools.php"><span>买卖小助手</span></a></li>
                     <li><a href="contact.php"><span>联系我们</span></a></li>
                 </ul>
             </div>
