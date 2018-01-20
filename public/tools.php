e01e265704b3b60cc7ff7314d20552b84988c74d
Jinyue Song <jsong32@dons.usfca.edu>
2018-01-19 16:16:04 -0800

    trouble in calculator

--- public/tools.php
+++ public/tools.php
@@ -1,22 +1,58 @@
 <?php include 'shared/header.php';?>
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script src="https://cdn.rawgit.com/chrisvfritz/5f0a639590d6e648933416f90ba7ae4e/raw/974aa47f8f9c5361c5233bd56be37db8ed765a09/currency-validator.js"></script>
+
   <div class="content">
     <div class="content_resize">
       <div class="mainbar">
         <div class="article">
-          <h2><span>买卖小助手</span></h2>
+          <h2><span></span>买卖小助手</h2>
           <div class="clr"></div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Suspendisse nulla ligula, blandit ultricies aliquet ac, lobortis in massa. Nunc dolor sem, tincidunt vitae viverra in, egestas sed lacus.</strong> Etiam in ullamcorper felis. Nulla cursus feugiat leo, ut dictum metus semper a. Vivamus euismod, arcu gravida sollicitudin vestibulum, quam sem tempus quam, quis ullamcorper erat nunc in massa. Donec aliquet ante non diam sollicitudin quis auctor velit sodales. Morbi neque est, posuere at fringilla non, mollis nec nibh. Sed commodo tortor nec sem tincidunt mattis. Nam convallis aliquam nibh eu luctus. Nunc vel tincidunt lacus. Suspendisse sit amet pulvinar ante.</p>
-          <p>Phasellus diam justo, laoreet vel vulputate eu, congue vel est. Maecenas eros libero, sollicitudin a vulputate fermentum, ultrices vel lacus. Nam in metus non augue fermentum consequat ultrices ac enim. Integer aliquam urna non diam aliquam eget hendrerit sem molestie.</p>
-          <p><strong>Lorem ipsum dolor sit amet</strong></p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget bibendum tellus. Nunc vel imperdiet tellus. Mauris ornare aliquam urna, accumsan bibendum eros auctor ac.</p>
-          <ul class="sb_menu">
-            <li><a href="#"><strong>Lorem ipsum</strong></a></li>
-            <li><a href="#"><strong>Lorem ipsum</strong></a></li>
-            <li><a href="#"><strong>Lorem ipsum</strong></a></li>
-            <li><a href="#"><strong>Lorem ipsum</strong></a></li>
-            <li><a href="#"><strong>Lorem ipsum</strong></a></li>
-            <li><a href="#"><strong>Lorem ipsum</strong></a></li>
-          </ul>
+            <h2>贷款计算器</h2>
+            <p><span><strong>贷款计算器</strong></span></p>
+            <div class="article" id="calculator">
+                <div class="clr"></div>
+                
+                <el-form ref="form" :model="form" label-width="120px" v-loading="sendLoading" :rules="rules">
+                    <el-form-item label="房产价格" prop="price">
+                        <el-input v-model="form.price"></el-input>
+                    </el-form-item>
+                    <el-form-item label="首付" prop="downPayment">
+                        <el-input v-model="form.downPayment"></el-input>
+                    </el-form-item>
+                    <el-form-item label="贷款年限" prop="term">
+                        <el-input v-model="form.term"></el-input>
+                    </el-form-item>
+                    <el-form-item label="贷款利率" prop="interestRate">
+                        <el-input v-model="form.interestRate"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="calculate()">Calculate</el-button>
+                    </el-form-item>
+                </el-form>
+                
+                <div id="app">
+                    <currency-input 
+                        label="房产价格" 
+                        v-model="price"
+                    ></currency-input>
+                      <currency-input 
+                        label="首付" 
+                        v-model="downPayment"
+                      ></currency-input>
+                      <currency-input 
+                        label="贷款年限" 
+                        v-model="term"
+                      ></currency-input>
+                      <currency-input 
+                        label="贷款利率" 
+                        v-model="interestRate"
+                      ></currency-input>
+  
+                        <p>月还贷: ${{ total }}</p>
+                </div>
+                
+            </div>
         </div>
       </div>
         <?php include 'shared/sidebar.php';?>
@@ -24,3 +60,72 @@
     </div>
   </div>
 <?php include 'shared/footer.php';?>
+
+<script>
+Vue.component('currency-input', {
+  template: '\
+    <div>\
+      <label v-if="label">{{ label }}</label>\
+      $\
+      <input\
+        ref="input"\
+        v-bind:value="value"\
+        v-on:input="updateValue($event.target.value)"\
+        v-on:focus="selectAll"\
+        v-on:blur="formatValue"\
+      >\
+    </div>\
+  ',
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted: function () {
+    this.formatValue()
+  },
+  methods: {
+    updateValue: function (value) {
+      var result = currencyValidator.parse(value, this.value)
+      if (result.warning) {
+        this.$refs.input.value = result.value
+      }
+      this.$emit('input', result.value)
+    },
+    formatValue: function () {
+      this.$refs.input.value = currencyValidator.format(this.value)
+    },
+    selectAll: function (event) {
+      // Workaround for Safari bug
+      // http://stackoverflow.com/questions/1269722/selecting-text-on-focus-using-jquery-not-working-in-safari-and-chrome
+      setTimeout(function () {
+      	event.target.select()
+      }, 0)
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    price: 0,
+    downPayment: 0,
+    term: 0,
+    interestRate: 0
+  },
+  computed: {
+    total: function () {
+      return (
+        (this.price - this.downPayment) / (  
+        ((1 + this.interestRate)^(this.term*12) - 1) / (this.interestRate * (1 + this.interestRate) ^ (this.term*12))
+      )
+      )
+    }
+  }
+})
+</script>
