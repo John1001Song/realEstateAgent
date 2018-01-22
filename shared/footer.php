cc00c2d2f58b5c48118fc8f302db211b61885c49
Jinyue Song <jsong32@dons.usfca.edu>
2018-01-21 16:51:14 -0800

    header and footer photo update

--- shared/footer.php
+++ shared/footer.php
@@ -2,16 +2,15 @@
     <div class="fbg_resize">
         <div class="col c1">
             <h2><span>Other </span> houses</h2>
-            <a href="#"><img src="images/gal1.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal2.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal3.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/gal6.jpg" width="75" height="75" alt="" class="gal" /></a> 
+            <a href="#"><img src="images/footerPhoto1.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/footerPhoto2.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/footerPhoto3.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/footerPhoto4.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/footerPhoto5.jpg" width="75" height="75" alt="" class="gal" /></a> <a href="#"><img src="images/footerPhoto6.jpg" width="75" height="75" alt="" class="gal" /></a> 
         </div>
         <div class="col c2">
             <h2><span>Contact</span> Us</h2>
             <p>联系方式</p>
-            <p class="contact_info"> <span>Address:</span> 1458 TemplateAccess, USA<br />
-                <span>Telephone:</span> +123-1234-5678<br />
-                <span>FAX:</span> +458-4578<br />
-                <span>微信:</span> +301 - 0125 - 01258<br />
-                <span>E-mail:</span> <a href="#">mail@yoursitename.com</a> 
+            <p class="contact_info"> <span>Address:</span> 123 TemplateAccess, ON Canada<br />
+                <span>Telephone:</span> +1 (647) 988-1390<br />
+                <span>微信:</span> Jeanhwca<br />
+                <span>E-mail:</span> <a href="#">huangjingca@gmail.com</a> 
             </p>
         </div>
         <div class="col c3">
