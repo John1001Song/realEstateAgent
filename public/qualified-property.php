a865075cdf995465cb090949e84c4e59a40fd774
Jinyue Song <jsong32@dons.usfca.edu>
2018-02-09 23:05:20 -0800

    qualified property page in process

--- public/qualified-property.php
+++ public/qualified-property.php
@@ -6,16 +6,40 @@
           <h2><span>High Quality Properties</span></h2>
           <div class="clr"></div>
           <p>近期上市房产</p>
-          <div class="">
             <ul class="qualified-property-list">
                 <li class="property-in-list">
-                
+                    <div class="property-photo-list">
+                        <img src="images/condo_ten_york.jpg">
+                    </div>
+                    <div class="property-features-list">
+                        <dl class="property-price-list">
+                            <dt><strong>$</strong>1,328,000</dt>
+                        </dl>
+                        <dl class="property-address-list">
+                            <dt><strong>Address</strong>: 10 York St, Toronto, ON M5J 2L9</dt>
+                        </dl>
+                        <dl class="property-space-list">
+                            <dt><strong>Space</strong>: 1305 sq ft</dt>
+                        </dl>
+                        <dl class="property-bedroom-list">
+                            <dt><strong>Bedroom</strong>: 3</dt>
+                        </dl>
+                        <dl class="property-bathroom-list">
+                            <dl><strong>Bathroom</strong>: 2</dl>
+                        </dl>
+                    </div>
+                    <div class="property-list-description">
+                        <p>TEN YORK 位于多伦多市中心，频临安大略湖的地标性建筑，由著名建筑商 TRIDEL和多伦多市政府打造，公寓坐落于多伦多市中心湖滨的中央地带, 位于Harbour/York交界处, 最佳的地段, 最好的建筑商。无论是自住/投资都是AAA地段.，极具增值潜力。大楼共75层， 774 个单元。<br/>
+                            豪华装修：9 尺平滑楼顶，高密度工程复合地板，大理石台面，镶入式电器<br/>
+                            建筑设计: Wallman Architects<br/> 
+                            室内设计: II BY IV Design Associates<br/>
+                        此单元是在32层的，三房三卫，明亮通透，饱览湖景和市区景观，举步联合地铁站，市中心金融街，湖滨，地下商城，冰球名人馆，BCE购物广场，Sony剧院，Rogers Center, CN Tower，水族馆，加航中心, 多伦多会议中心，地铁，高速等。</p>
+                    </div>
                 </li>  
             </ul>  
-          </div>
           
             
-          <p>更多房产请联系<a>Huang Jing</a></p>
+<!--          <p>更多房产请联系<a>Huang Jing</a></p>-->
         </div>
       </div>
         <?php include 'shared/sidebar.php';?>
